0b576bb7ab58400982bbba53b4b49a6baae5e55d
Stephen Wilkins <s.wilkins@sussex.ac.uk>
2022-09-29 13:34:22 +0100

    updated

--- generate_templates/generate_templates.py
+++ generate_templates/generate_templates.py
@@ -112,25 +112,25 @@ class TemplateGenerator:
 
 if __name__ == '__main__':
 
-    template_set_name = 'Wilkins22-v0.2'
+    template_set_name = 'Wilkins22-v0.5'
 
     sps_grids = [
-    'bc03_chabrier03',
-    'bpass-v2.2.1-bin_100-100',
-    'bpass-v2.2.1-bin_100-300',
-    'bpass-v2.2.1-bin_135-100',
-    'bpass-v2.2.1-bin_135-300',
-    'bpass-v2.2.1-bin_135all-100',
-    'bpass-v2.2.1-bin_170-100',
-    'bpass-v2.2.1-bin_170-300',
+    # 'bc03_chabrier03',
+    # 'bpass-v2.2.1-bin_100-100',
+    # 'bpass-v2.2.1-bin_100-300',
+    # 'bpass-v2.2.1-bin_135-100',
+    # 'bpass-v2.2.1-bin_135-300',
+    # 'bpass-v2.2.1-bin_135all-100',
+    # 'bpass-v2.2.1-bin_170-100',
+    # 'bpass-v2.2.1-bin_170-300',
     'fsps-v3.2_Chabrier03',
-    'bpass-v2.2.1-bin_chab-100',
-    'bpass-v2.2.1-bin_chab-300',
-    'maraston-rhb_kroupa',
-    'maraston-rhb_salpeter',
-    'bc03-2016-Stelib_chabrier03',
-    'bc03-2016-BaSeL_chabrier03',
-    'bc03-2016-Miles_chabrier03',
+    # 'bpass-v2.2.1-bin_chab-100',
+    # 'bpass-v2.2.1-bin_chab-300',
+    # 'maraston-rhb_kroupa',
+    # 'maraston-rhb_salpeter',
+    # 'bc03-2016-Stelib_chabrier03',
+    # 'bc03-2016-BaSeL_chabrier03',
+    # 'bc03-2016-Miles_chabrier03',
     ]
     cloudy_grid = 'cloudy-v17.03_log10Uref-2'
     Z = 0.01
@@ -141,33 +141,62 @@ if __name__ == '__main__':
 
         te = TemplateGenerator(sps_grid, cloudy_grid, template_set_name = template_set_name)
 
-        # v0.2
-        te.generate_constant_galaxy(duration = 10 * Myr, Z = 0.0001, fesc = 0.0, fesc_LyA = 1.0, tauV = 0.0) # 10 Myr + nebular + LyA + no dust
-        te.generate_constant_galaxy(duration = 10 * Myr, Z = 0.0001, fesc = 0.0, fesc_LyA = 0.0, tauV = 0.0) # 10 Myr + nebular + no LyA + no dust
-        te.generate_constant_galaxy(duration = 10 * Myr, Z = 0.0001, fesc = 1.0, fesc_LyA = 1.0, tauV = 0.0) # 10 Myr + no nebular + no dust
+        # # v0.2
+        # te.generate_constant_galaxy(duration = 10 * Myr, Z = 0.0001, fesc = 0.0, fesc_LyA = 1.0, tauV = 0.0) # 10 Myr + nebular + LyA + no dust
+        # te.generate_constant_galaxy(duration = 10 * Myr, Z = 0.0001, fesc = 0.0, fesc_LyA = 0.0, tauV = 0.0) # 10 Myr + nebular + no LyA + no dust
+        # te.generate_constant_galaxy(duration = 10 * Myr, Z = 0.0001, fesc = 1.0, fesc_LyA = 1.0, tauV = 0.0) # 10 Myr + no nebular + no dust
+        #
+        # # v0.1, v0.2
+        # te.generate_constant_galaxy(duration = 10 * Myr, Z = 0.001, fesc = 0.0, fesc_LyA = 1.0, tauV = 0.0) # 10 Myr + nebular + LyA + no dust
+        # te.generate_constant_galaxy(duration = 10 * Myr, Z = 0.001, fesc = 0.0, fesc_LyA = 0.0, tauV = 0.0) # 10 Myr + nebular + no LyA + no dust
+        # te.generate_constant_galaxy(duration = 10 * Myr, Z = 0.001, fesc = 1.0, fesc_LyA = 1.0, tauV = 0.0) # 10 Myr + no nebular + no dust
+        #
+        # # v0.1, v0.2, v0.3
+        # te.generate_constant_galaxy(duration = 10 * Myr, Z = Z, fesc = 0.0, fesc_LyA = 1.0, tauV = 0.0) # 10 Myr + nebular + LyA + no dust
+        # te.generate_constant_galaxy(duration = 10 * Myr, Z = Z, fesc = 0.0, fesc_LyA = 0.0, tauV = 0.0) # 10 Myr + nebular + no LyA + no dust
+        # te.generate_constant_galaxy(duration = 10 * Myr, Z = Z, fesc = 1.0, fesc_LyA = 1.0, tauV = 0.0) # 10 Myr + no nebular + no dust
+        #
+        # # --- 100 Myr templates with dust and nebular emission
+        #
+        # # v0.1, v0.2, v0.3
+        # for tauV in [0.0, 0.3, 1.0, 3.0]:
+        #     te.generate_constant_galaxy(duration = 100 * Myr, Z = Z, fesc = 0.0, fesc_LyA = 1.0, tauV = tauV)
+        #
+        # # --- instantaneous bursts
+        #
+        # # v0.1, v0.2, v0.3
+        # for log10age in [8.0, 8.5, 9.0, 9.5, 10.0]:
+        #     te.generate_instant_galaxy(log10age = log10age, Z = Z)
 
-        # v0.1, v0.2
-        te.generate_constant_galaxy(duration = 10 * Myr, Z = 0.001, fesc = 0.0, fesc_LyA = 1.0, tauV = 0.0) # 10 Myr + nebular + LyA + no dust
-        te.generate_constant_galaxy(duration = 10 * Myr, Z = 0.001, fesc = 0.0, fesc_LyA = 0.0, tauV = 0.0) # 10 Myr + nebular + no LyA + no dust
-        te.generate_constant_galaxy(duration = 10 * Myr, Z = 0.001, fesc = 1.0, fesc_LyA = 1.0, tauV = 0.0) # 10 Myr + no nebular + no dust
+        # v0.4
+        # for Z in [0.001, 0.01]:
+        #     te.generate_constant_galaxy(duration = 10 * Myr, Z = Z, fesc = 0.0, fesc_LyA = 1.0, tauV = 0.0) # 10 Myr + nebular + LyA + no dust
+        #     te.generate_constant_galaxy(duration = 10 * Myr, Z = Z, fesc = 0.0, fesc_LyA = 0.0, tauV = 0.0) # 10 Myr + nebular + no LyA + no dust
+        #     te.generate_constant_galaxy(duration = 10 * Myr, Z = Z, fesc = 1.0, fesc_LyA = 1.0, tauV = 0.0) # 10 Myr + no nebular + no dust
+        #
+        #     # --- 100 Myr templates with dust and nebular emission
+        #     for tauV in [0.0, 0.3, 1.0, 3.0]:
+        #         te.generate_constant_galaxy(duration = 100 * Myr, Z = Z, fesc = 0.0, fesc_LyA = 1.0, tauV = tauV)
+        #
+        #     # --- instantaneous bursts
+        #     for log10age in [8.0, 8.5, 9.0, 9.5, 10.0]:
+        #         te.generate_instant_galaxy(log10age = log10age, Z = Z)
 
-        # v0.1, v0.2, v0.3
-        te.generate_constant_galaxy(duration = 10 * Myr, Z = Z, fesc = 0.0, fesc_LyA = 1.0, tauV = 0.0) # 10 Myr + nebular + LyA + no dust
-        te.generate_constant_galaxy(duration = 10 * Myr, Z = Z, fesc = 0.0, fesc_LyA = 0.0, tauV = 0.0) # 10 Myr + nebular + no LyA + no dust
-        te.generate_constant_galaxy(duration = 10 * Myr, Z = Z, fesc = 1.0, fesc_LyA = 1.0, tauV = 0.0) # 10 Myr + no nebular + no dust
+        # v0.5
+        for Z in [0.0001, 0.01]:
+            te.generate_constant_galaxy(duration = 10 * Myr, Z = Z, fesc = 0.0, fesc_LyA = 1.0, tauV = 0.0) # 10 Myr + nebular + LyA + no dust
+            te.generate_constant_galaxy(duration = 10 * Myr, Z = Z, fesc = 0.0, fesc_LyA = 0.0, tauV = 0.0) # 10 Myr + nebular + no LyA + no dust
+            te.generate_constant_galaxy(duration = 10 * Myr, Z = Z, fesc = 1.0, fesc_LyA = 1.0, tauV = 0.0) # 10 Myr + no nebular + no dust
 
-        # --- 100 Myr templates with dust and nebular emission
+            # --- 100 Myr templates with dust and nebular emission
+            for tauV in [0.0, 0.3, 1.0, 3.0]:
+                te.generate_constant_galaxy(duration = 100 * Myr, Z = Z, fesc = 0.0, fesc_LyA = 1.0, tauV = tauV)
 
-        # v0.1, v0.2, v0.3
-        for tauV in [0.0, 0.3, 1.0, 3.0]:
+            # --- instantaneous bursts
+            for log10age in [8.0, 8.5, 9.0, 9.5, 10.0]:
+                te.generate_instant_galaxy(log10age = log10age, Z = Z)
 
-            te.generate_constant_galaxy(duration = 100 * Myr, Z = Z, fesc = 0.0, fesc_LyA = 1.0, tauV = tauV)
 
-        # --- instantaneous bursts
 
-        # v0.1, v0.2, v0.3
-        for log10age in [8.0, 8.5, 9.0, 9.5, 10.0]:
-
-            te.generate_instant_galaxy(log10age = log10age, Z = Z)
 
         te.write_parameter_file()
